4b0e608d21faf3d1df8cec5c3b7b33f7884d3f19
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-03 21:11:16 -0500

    add Song model and two starter seed objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Song.create(title: "Sitting by the dock of the bay", album: "The Dock of the Bay", artist: "Otis Redding", year:1968)
+
+Song.create(
+  title: "Achy Breaky Heart", 
+  album: "Some Gave All", 
+  artist: "Billy Ray Cyrus", 
+  year:1995
+)
+
+Song.create(
+  title: "What a Wonderful World", 
+  album: "What a Wonderful World", 
+  artist: "Louis Armstrong", 
+  year:1967
+)
