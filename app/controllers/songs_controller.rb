1a1840db1c7bd6cc4d3a013f991fd74a50199c13
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-03 21:55:26 -0500

    add create, show, udpate, and delete actions with tests

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,4 +3,36 @@ class SongsController < ApplicationController
     @songs = Song.all
     render :index
   end
+
+  def create
+    @song = Song.create( 
+      title: "#{params[:title]}", 
+      album: "#{params[:album]}", 
+      artist: "#{params[:artist]}", 
+      year: params[:year]
+    )
+    render :show
+  end
+
+  def show
+    @song = Song.find_by(id: "#{params[:id]}")
+    render :show
+  end
+
+  def update
+    @song = Song.find_by(id: "#{params[:id]}")
+    @song.update(
+      title: params[:title] || @song.title, 
+      album: params[:album] || @song.album, 
+      artist: params[:artist] || @song.artist, 
+      year: params[:year] || @song.year
+    )
+    render :show
+  end
+
+  def destroy
+    @song = Song.find_by(id: "#{params[:id]}")
+    @song.destroy
+    render json: {result: "#{@song.title} successfully destroyed"}
+  end
 end
--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class SongsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/songs.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Song.count, data.length
+
+  end
+
+  test "create" do
+    assert_difference "Song.count", 1 do
+      post "/songs.json", params: {name: "test", album: "test", artist: "test", year: 1999}
+      assert_response 200
+    end
+  end
+
+  test "show" do
+    get "/songs/#{Song.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "title", "album", "artist", "year"], data.keys
+  end
+
+  test "update" do
+    patch "/songs/#{Song.first.id}.json", params: {title: "new one"}
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Song.first.title, "new one"
+  end
+
+  test "destroy" do
+    assert_difference "Song.count", -1 do
+      delete "/songs/#{Song.first.id}.json"
+      assert_response 200
+    end
+  end
 end
